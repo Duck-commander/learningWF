767ae672a3306049d5fee76ccfbdea0f36fab1c7
Duck_commander <vespall2910@gmail.com>
2023-10-05 00:04:04 +0300

    Добавьте файлы проекта.

---
+++ learningWF/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace learningWF
+{
+    partial class programForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.d4Count = new System.Windows.Forms.NumericUpDown();
+            this.programName = new System.Windows.Forms.Label();
+            this.d4Text = new System.Windows.Forms.Label();
+            this.d6Text = new System.Windows.Forms.Label();
+            this.d6Count = new System.Windows.Forms.NumericUpDown();
+            this.d8Text = new System.Windows.Forms.Label();
+            this.d8Count = new System.Windows.Forms.NumericUpDown();
+            this.d10Text = new System.Windows.Forms.Label();
+            this.d10Count = new System.Windows.Forms.NumericUpDown();
+            this.d12Text = new System.Windows.Forms.Label();
+            this.d12Count = new System.Windows.Forms.NumericUpDown();
+            this.d20Text = new System.Windows.Forms.Label();
+            this.d20Count = new System.Windows.Forms.NumericUpDown();
+            this.d100Text = new System.Windows.Forms.Label();
+            this.d100Count = new System.Windows.Forms.NumericUpDown();
+            this.resultText = new System.Windows.Forms.Label();
+            this.sumResultText = new System.Windows.Forms.Label();
+            this.diseThrowBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.d4Count)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d6Count)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d8Count)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d10Count)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d12Count)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d20Count)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d100Count)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // d4Count
+            // 
+            this.d4Count.AutoSize = true;
+            this.d4Count.Location = new System.Drawing.Point(45, 83);
+            this.d4Count.Name = "d4Count";
+            this.d4Count.Size = new System.Drawing.Size(48, 22);
+            this.d4Count.TabIndex = 0;
+            // 
+            // programName
+            // 
+            this.programName.AutoSize = true;
+            this.programName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.programName.ForeColor = System.Drawing.Color.Firebrick;
+            this.programName.Location = new System.Drawing.Point(325, 25);
+            this.programName.Name = "programName";
+            this.programName.Size = new System.Drawing.Size(123, 22);
+            this.programName.TabIndex = 1;
+            this.programName.Text = "Dice thrower";
+            this.programName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // d4Text
+            // 
+            this.d4Text.AutoSize = true;
+            this.d4Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.d4Text.Location = new System.Drawing.Point(116, 83);
+            this.d4Text.Name = "d4Text";
+            this.d4Text.Size = new System.Drawing.Size(33, 20);
+            this.d4Text.TabIndex = 2;
+            this.d4Text.Text = "D4";
+            // 
+            // d6Text
+            // 
+            this.d6Text.AutoSize = true;
+            this.d6Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.d6Text.Location = new System.Drawing.Point(116, 123);
+            this.d6Text.Name = "d6Text";
+            this.d6Text.Size = new System.Drawing.Size(33, 20);
+            this.d6Text.TabIndex = 4;
+            this.d6Text.Text = "D6";
+            // 
+            // d6Count
+            // 
+            this.d6Count.AutoSize = true;
+            this.d6Count.Location = new System.Drawing.Point(45, 123);
+            this.d6Count.Name = "d6Count";
+            this.d6Count.Size = new System.Drawing.Size(48, 22);
+            this.d6Count.TabIndex = 3;
+            // 
+            // d8Text
+            // 
+            this.d8Text.AutoSize = true;
+            this.d8Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.d8Text.Location = new System.Drawing.Point(116, 164);
+            this.d8Text.Name = "d8Text";
+            this.d8Text.Size = new System.Drawing.Size(33, 20);
+            this.d8Text.TabIndex = 6;
+            this.d8Text.Text = "D8";
+            // 
+            // d8Count
+            // 
+            this.d8Count.AutoSize = true;
+            this.d8Count.Location = new System.Drawing.Point(45, 164);
+            this.d8Count.Name = "d8Count";
+            this.d8Count.Size = new System.Drawing.Size(48, 22);
+            this.d8Count.TabIndex = 5;
+            // 
+            // d10Text
+            // 
+            this.d10Text.AutoSize = true;
+            this.d10Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.d10Text.Location = new System.Drawing.Point(116, 204);
+            this.d10Text.Name = "d10Text";
+            this.d10Text.Size = new System.Drawing.Size(43, 20);
+            this.d10Text.TabIndex = 8;
+            this.d10Text.Text = "D10";
+            // 
+            // d10Count
+            // 
+            this.d10Count.AutoSize = true;
+            this.d10Count.Location = new System.Drawing.Point(45, 204);
+            this.d10Count.Name = "d10Count";
+            this.d10Count.Size = new System.Drawing.Size(48, 22);
+            this.d10Count.TabIndex = 7;
+            // 
+            // d12Text
+            // 
+            this.d12Text.AutoSize = true;
+            this.d12Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.d12Text.Location = new System.Drawing.Point(116, 244);
+            this.d12Text.Name = "d12Text";
+            this.d12Text.Size = new System.Drawing.Size(43, 20);
+            this.d12Text.TabIndex = 10;
+            this.d12Text.Text = "D12";
+            // 
+            // d12Count
+            // 
+            this.d12Count.AutoSize = true;
+            this.d12Count.Location = new System.Drawing.Point(45, 244);
+            this.d12Count.Name = "d12Count";
+            this.d12Count.Size = new System.Drawing.Size(48, 22);
+            this.d12Count.TabIndex = 9;
+            // 
+            // d20Text
+            // 
+            this.d20Text.AutoSize = true;
+            this.d20Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.d20Text.Location = new System.Drawing.Point(116, 290);
+            this.d20Text.Name = "d20Text";
+            this.d20Text.Size = new System.Drawing.Size(43, 20);
+            this.d20Text.TabIndex = 12;
+            this.d20Text.Text = "D20";
+            // 
+            // d20Count
+            // 
+            this.d20Count.AutoSize = true;
+            this.d20Count.Location = new System.Drawing.Point(45, 290);
+            this.d20Count.Name = "d20Count";
+            this.d20Count.Size = new System.Drawing.Size(48, 22);
+            this.d20Count.TabIndex = 11;
+            // 
+            // d100Text
+            // 
+            this.d100Text.AutoSize = true;
+            this.d100Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.d100Text.Location = new System.Drawing.Point(116, 334);
+            this.d100Text.Name = "d100Text";
+            this.d100Text.Size = new System.Drawing.Size(53, 20);
+            this.d100Text.TabIndex = 14;
+            this.d100Text.Text = "D100";
+            // 
+            // d100Count
+            // 
+            this.d100Count.AutoSize = true;
+            this.d100Count.Location = new System.Drawing.Point(45, 334);
+            this.d100Count.Name = "d100Count";
+            this.d100Count.Size = new System.Drawing.Size(48, 22);
+            this.d100Count.TabIndex = 13;
+            // 
+            // resultText
+            // 
+            this.resultText.AutoSize = true;
+            this.resultText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultText.Location = new System.Drawing.Point(41, 379);
+            this.resultText.Name = "resultText";
+            this.resultText.Size = new System.Drawing.Size(66, 22);
+            this.resultText.TabIndex = 15;
+            this.resultText.Text = "Result:";
+            // 
+            // sumResultText
+            // 
+            this.sumResultText.AutoSize = true;
+            this.sumResultText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sumResultText.Location = new System.Drawing.Point(113, 379);
+            this.sumResultText.MaximumSize = new System.Drawing.Size(500, 0);
+            this.sumResultText.Name = "sumResultText";
+            this.sumResultText.Size = new System.Drawing.Size(0, 22);
+            this.sumResultText.TabIndex = 16;
+            // 
+            // diseThrowBtn
+            // 
+            this.diseThrowBtn.BackColor = System.Drawing.Color.LightCoral;
+            this.diseThrowBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.diseThrowBtn.Location = new System.Drawing.Point(628, 379);
+            this.diseThrowBtn.Name = "diseThrowBtn";
+            this.diseThrowBtn.Size = new System.Drawing.Size(125, 38);
+            this.diseThrowBtn.TabIndex = 17;
+            this.diseThrowBtn.Text = "Throw Dice";
+            this.diseThrowBtn.UseVisualStyleBackColor = false;
+            this.diseThrowBtn.Click += new System.EventHandler(this.diseThrowBtn_Click);
+            // 
+            // programForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.diseThrowBtn);
+            this.Controls.Add(this.sumResultText);
+            this.Controls.Add(this.resultText);
+            this.Controls.Add(this.d100Text);
+            this.Controls.Add(this.d100Count);
+            this.Controls.Add(this.d20Text);
+            this.Controls.Add(this.d20Count);
+            this.Controls.Add(this.d12Text);
+            this.Controls.Add(this.d12Count);
+            this.Controls.Add(this.d10Text);
+            this.Controls.Add(this.d10Count);
+            this.Controls.Add(this.d8Text);
+            this.Controls.Add(this.d8Count);
+            this.Controls.Add(this.d6Text);
+            this.Controls.Add(this.d6Count);
+            this.Controls.Add(this.d4Text);
+            this.Controls.Add(this.programName);
+            this.Controls.Add(this.d4Count);
+            this.Name = "programForm";
+            this.Text = "Dice thrower";
+            ((System.ComponentModel.ISupportInitialize)(this.d4Count)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d6Count)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d8Count)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d10Count)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d12Count)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d20Count)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d100Count)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown d4Count;
+        private System.Windows.Forms.Label programName;
+        private System.Windows.Forms.Label d4Text;
+        private System.Windows.Forms.Label d6Text;
+        private System.Windows.Forms.NumericUpDown d6Count;
+        private System.Windows.Forms.Label d8Text;
+        private System.Windows.Forms.NumericUpDown d8Count;
+        private System.Windows.Forms.Label d10Text;
+        private System.Windows.Forms.NumericUpDown d10Count;
+        private System.Windows.Forms.Label d12Text;
+        private System.Windows.Forms.NumericUpDown d12Count;
+        private System.Windows.Forms.Label d20Text;
+        private System.Windows.Forms.NumericUpDown d20Count;
+        private System.Windows.Forms.Label d100Text;
+        private System.Windows.Forms.NumericUpDown d100Count;
+        private System.Windows.Forms.Label resultText;
+        private System.Windows.Forms.Label sumResultText;
+        private System.Windows.Forms.Button diseThrowBtn;
+    }
+}
+
---
+++ learningWF/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learningWF
+{
+    public partial class programForm : Form
+    {
+        Random rnd = new Random();
+
+        public programForm()
+        {
+            InitializeComponent();
+        }
+
+        private void diseThrowBtn_Click(object sender, EventArgs e)
+        {
+            string textThrow = "";
+            int sumThrow = 0;
+            int rndCount;
+
+            int[] typeDice = { 4, 6, 8, 10, 12, 20, 100 };
+            int[] disesCount = new int[] {(int)d4Count.Value, (int)d6Count.Value, (int)d8Count.Value,
+                (int)d10Count.Value, (int)d12Count.Value, (int)d20Count.Value, (int)d100Count.Value};
+
+            for (int i = 0; i < disesCount.Length; i++) 
+            {
+                if (disesCount[i] > 0)
+                {
+                    if (textThrow != "") textThrow += "+ ";
+
+                    textThrow += $"({disesCount[i]}D{typeDice[i]}) ";
+
+                    for (int j = 0; j < disesCount[i]; j++)
+                    {
+                        rndCount = rnd.Next(1, typeDice[i]);
+                        sumThrow += rndCount;
+                        textThrow += $"{rndCount} {(j == disesCount[i] - 1 ? "" : " + ")}";
+                    }
+
+                    //textThrow += $") ";
+                }
+            }
+
+            textThrow += $"= {sumThrow}";
+            
+            sumResultText.Text = textThrow;
+        }
+    }
+}
